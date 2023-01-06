bd897a8a37675c33c7d1c0e76e2445e5731d0d2e
IKILLERx2x <74920170+IKILLERx2x@users.noreply.github.com>
2023-01-06 21:04:56 +0300

    final

---
+++ mi_card/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(color: Colors.red, height: double.infinity, width: 100),
+              Container(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.green,
+                      ),
+                    ]),
+              ),
+              Container(
+                color: Colors.blue,
+                height: double.infinity,
+                width: 100,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
