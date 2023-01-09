4456afdff46e2bc8fab15970311d8105d96ce86b
IKILLERx2x <74920170+IKILLERx2x@users.noreply.github.com>
2023-01-09 20:44:21 +0300

    final

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,14 +1,55 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballImageNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue.shade500,
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballImageNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset("images/ball$ballImageNumber.png"),
+        ),
+      ),
+    );
   }
 }
